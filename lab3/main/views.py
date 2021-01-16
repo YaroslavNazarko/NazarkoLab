9966f868b0fa9cd2c238e618650d21fb4884a820
ReCursed <yarko.nazarko@gmail.com>
2021-01-17 01:50:17 +0200

    added main.html and urls.py

---
+++ lab3/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):    
+    response = {'date': datetime.today(), 'current_page': request.path, 'server_info': os.name, 'client_info': request.META['HTTP_USER_AGENT']}
+    return JsonResponse(response)
+
